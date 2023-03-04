26d33e497a4d1d263b7871740b58515de72d9ebd
dz365 <dwqz365@gmail.com>
2023-03-04 13:19:21 -0500

    Addedsample data points

--- frontend/src/pages/ExplorePage.tsx
+++ frontend/src/pages/ExplorePage.tsx
@@ -1,5 +1,8 @@
 import { VisLeafletMap } from "@unovis/react";
+import { useCallback } from "react";
 import Navbar from "../navigation/Navbar";
+
+import { MapPointDataRecord, data } from "./SampleData";
 
 const ExplorePage = () => {
   const style = `https://api.maptiler.com/maps/outdoor-v2/style.json?key=S045gSdAQ3IN2GgxxGWu`;
@@ -8,11 +11,28 @@ const ExplorePage = () => {
     `<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>`,
   ];
 
+  const pointLatitude = useCallback((d: MapPointDataRecord) => d.latitude, []);
+  const pointLongitude = useCallback(
+    (d: MapPointDataRecord) => d.longitude,
+    []
+  );
+  const pointBottomLabel = useCallback((d: MapPointDataRecord) => d.id, []);
+  const pointColor = "#286e47";
+
   return (
     <div className="min-h-screen bg-green-100">
       <Navbar />
       <div className="relative w-full h-screen">
-        <VisLeafletMap style={style} attribution={attribution} />
+        <VisLeafletMap
+          style={style}
+          attribution={attribution}
+          data={data}
+          pointLatitude={pointLatitude}
+          pointLongitude={pointLongitude}
+          pointBottomLabel={pointBottomLabel}
+          pointColor={pointColor}
+          clusterExpandOnClick={false}
+        />
       </div>
     </div>
   );
