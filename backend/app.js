e1452f789afffc1b10e5f2bdab748a47c721a74e
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-03-20 21:50:40 -0400

    Fix docker

--- backend/app.js
+++ backend/app.js
@@ -12,13 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(authError);
 
-try {
-  await sequelize.authenticate();
-  await sequelize.sync({ alter: { drop: false } });
-  console.log("Connection has been established successfully.");
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+await sequelize.authenticate();
+await sequelize.sync({ alter: { drop: false } });
 
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
