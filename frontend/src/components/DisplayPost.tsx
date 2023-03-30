1ec53463c194592b362edcbeded29697194a6542
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-03-30 18:42:29 -0400

    Avatar load error

--- frontend/src/components/DisplayPost.tsx
+++ frontend/src/components/DisplayPost.tsx
@@ -140,6 +140,10 @@ const DisplayPost: React.FC<DisplayPost> = ({ postId, userId }) => {
         <img
           src={`${process.env.REACT_APP_API_SERVER_URL}/users/${userId}/avatar`}
           alt="avatar"
+          onError={({ currentTarget }) => {
+            currentTarget.onerror = null; // prevents looping
+            currentTarget.src = "/default_avatar.png";
+          }}
           className="w-12 h-12 rounded-full border"
         />
         <div className="flex flex-col">
