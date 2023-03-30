d644ab8342535436be712ca914b10b4d09ca21cf
Daniel Zhang <65931281+dz365@users.noreply.github.com>
2023-03-30 11:18:30 -0400

    delete unused files

--- backend/reverseGeosearch.js
+++ backend/reverseGeosearch.js
@@ -1,5 +1,3 @@
-import { MAP_TILER_KEY } from "./api_keys.js";
-
 export const reverseGeoSearch = async (longitude, latitude) => {
   const geosearch = await fetch(
     `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}&zoom=15`
